fbc49094b3b935e98fb06e34f98254cabe497225
leandro braga <leandro@3upsolucoes.com.br>
2019-07-30 00:09:49 -0300

    Exemplo 12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Observable } from "rxjs";
 
 @Component({
@@ -6,73 +6,79 @@ import { Observable } from "rxjs";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  nomes: string[] = ["joao", "maria", "josé", "pedro", "felipe", "carlos"];
-  nomesFiltro: string[];
-  buscar(valor: string) {
-    this.nomesFiltro = this.nomes.filter(nome =>
-      nome.toLowerCase().includes(valor.toLowerCase())
-    );
-  }
+export class AppComponent implements OnInit {
+  observable: Observable<string>;
 
-  pessoas: any = [
-    { id: 1, nome: "joao", salario: 5000 },
-    { id: 2, nome: "maria", salario: 1000 },
-    { id: 3, nome: "josé", salario: 2000 },
-    { id: 4, nome: "pedro", salario: 3000 },
-    { id: 5, nome: "felipe", salario: 10000 },
-    { id: 6, nome: "carlos", salario: 800 }
-  ];
+  nomes: string[];
+  // nomesFiltro: string[];
+  // buscar(valor: string) {
+  //   this.nomesFiltro = this.nomes.filter(nome =>
+  //     nome.toLowerCase().includes(valor.toLowerCase())
+  //   );
+  // }
 
-  getValorTotal(): Number {
-    return this.pessoas.reduce((soma, pessoa) => soma + pessoa.salario, 0);
-  }
+  // pessoas: any = [
+  //   { id: 1, nome: "joao", salario: 5000 },
+  //   { id: 2, nome: "maria", salario: 1000 },
+  //   { id: 3, nome: "josé", salario: 2000 },
+  //   { id: 4, nome: "pedro", salario: 3000 },
+  //   { id: 5, nome: "felipe", salario: 10000 },
+  //   { id: 6, nome: "carlos", salario: 800 }
+  // ];
 
-  buscarId(id) {
-    return this.pessoas.find(pessoa => pessoa.id == id);
-  }
+  // getValorTotal(): Number {
+  //   return this.pessoas.reduce((soma, pessoa) => soma + pessoa.salario, 0);
+  // }
 
-  aumentarSalario(percentual) {
-    this.pessoas.map(
-      pessoa => (pessoa.salario += (pessoa.salario * percentual) / 100)
-    );
-  }
+  // buscarId(id) {
+  //   return this.pessoas.find(pessoa => pessoa.id == id);
+  // }
 
-  verificarSalario(valor: number) {
-    return this.pessoas.every(pessoa => pessoa.salario > valor);
-  }
+  // aumentarSalario(percentual) {
+  //   this.pessoas.map(
+  //     pessoa => (pessoa.salario += (pessoa.salario * percentual) / 100)
+  //   );
+  // }
 
-  buscaCampos(criterio: string) {
-    return this.pessoas.filter(pessoa =>
-      Object.keys(pessoa).some(chave =>
-        pessoa[chave].toString.includes(criterio)
-      )
-    );
-  }
+  // verificaSalario(valor: number) {
+  //   return this.pessoas.every(pessoa => pessoa.salario > valor);
+  // }
+
+  // buscaCampos(criterio: string) {
+  //   return this.pessoas.filter(pessoa =>
+  //     Object.keys(pessoa).some(chave =>
+  //       pessoa[chave].toString.includes(criterio)
+  //     )
+  //   );
+  // }
 
   ngOnInit() {
     const observable = new Observable(subscriber => {
-      subscriber.next(100);
-      subscriber.next(2);
-      subscriber.next(300);
-      setTimeout(() => {
-        subscriber.next(4);
-        subscriber.complete();
-      }, 1000);
+      setInterval(() => {
+        subscriber.next(this.makeid(5));
+      }, 10000);
     });
 
-    console.log("ASnteds de execurtar subscribe");
-    observable.subscribe({
+    let lista: Array<string> = [];
+    this.observable.subscribe({
       next(x) {
-        console.log("recebeu o valor " + x);
+        lista.push(x);
       },
       error(err) {
-        console.log("erro " + err);
-      },
-      complete() {
-        console.log("terminou o subscribe");
+        alert("ocorreu um erro" + err);
       }
     });
-    console.log("ultima linha");
+    this.nomes = lista;
+  }
+  enviar(valor: string) {
+    this.nomes.push(valor);
+  }
+  makeid(length) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVXZYWK0123456789";
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
   }
 }
