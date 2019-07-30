24baad4a94e03c2661b5f8f2581d8fb9c09496a7
leandro braga <leandro@3upsolucoes.com.br>
2019-07-29 23:34:37 -0300

    Exemplo 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,17 @@ export class AppComponent {
       nome.toLowerCase().includes(valor.toLowerCase())
     );
   }
+
+  pessoas: any = [
+    { id: 1, nome: "joao", salario: 5000 },
+    { id: 2, nome: "maria", salario: 1000 },
+    { id: 3, nome: "josé", salario: 2000 },
+    { id: 4, nome: "pedro", salario: 3000 },
+    { id: 5, nome: "felipe", salario: 10000 },
+    { id: 6, nome: "carlos", salario: 800 }
+  ];
+
+  getValorTotal(): Number {
+    return this.pessoas.reduce((soma, pessoa) => soma + pessoa.salario, 0);
+  }
 }
