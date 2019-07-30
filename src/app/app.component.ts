efb311c2e68a508a3410f3adc87004dd6b18d79c
leandro braga <leandro@3upsolucoes.com.br>
2019-07-29 23:23:42 -0300

    Exemplo 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  nomes: string[] = ['joao', 'maria', 'josé', 'pedro', 'felipe', 'carlos'];
+  nomes: string[] = ["joao", "maria", "josé", "pedro", "felipe", "carlos"];
+
+  buscar(valor: string) {
+    alert(valor);
+  }
 }
